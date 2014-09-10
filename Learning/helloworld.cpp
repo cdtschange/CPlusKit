e47878823cbe54c0379e84dd3ced98b23f0669c0
cdts_change <cdts_change@live.cn>
2014-09-10 21:34:51 +0800

    Create C++ Kit

---
+++ Learning/helloworld.cpp
@@ -0,0 +1,17 @@
+/*
+ * helloworld.cpp
+ *
+ *  Created on: 2014Äê9ÔÂ10ÈÕ
+ *      Author: Mao
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Hello world!"<<endl;
+	cin.get();
+	return 0;
+}
