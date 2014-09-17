5e4664c468151756345473aac04ad4efe23c211f
cdts_change <cdts_change@live.cn>
2014-09-17 16:02:57 +0800

    Add Vector class in Learning

---
+++ Learning/vector.h
@@ -0,0 +1,49 @@
+/*
+ * Vector
+ *
+ *  Created on: Sep 17, 2014
+ *      Author: cdts_change
+ */
+
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <iostream>
+namespace VECTOR
+{
+	class Vector
+	{
+	public:
+		enum Mode {RECT, POL}
+	private:
+		double x;
+		double y;
+		double mag;
+		double ang;
+		Mode mode;
+		void set_mag();
+		void set_ang();
+		void set_x();
+		void set_y();
+	public:
+		Vector();
+		Vector(double n1, double n2, Mode form = RECT);
+		void reset(double n1, double n2, Mode form = RECT);
+		~Vector();
+		doble xval() const {return x;}
+		doble yval() const {return y;}
+		doble magval() const {return mag;}
+		doble angval() const {return ang;}
+		void polar_mode();
+		void rect_mode();
+		Vector operator+(const Vector &b) const;
+		Vector operator-(const Vector &b) const;
+		Vector operator-() const;
+		Vector operator*(double n) const;
+		friend Vector operator*(double n, const Vector &a);
+		friend std::ostream & operator<<(std::ostream & os, const Vector &v);
+	};
+}
+
+
+#endif /* VECTOR_H */
