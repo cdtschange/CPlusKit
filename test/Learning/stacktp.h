28a698e9eecd881f8e6e23b432829346c3d5215c
cdts_change <cdts_change@live.cn>
2014-09-24 10:15:21 +0800

    add stack

---
+++ test/Learning/stacktp.h
@@ -0,0 +1,64 @@
+#ifndef STACKTP_H
+#define STACKTP_H
+
+template <class T>
+class Stack
+{
+private:
+	enum [MAX = 10];
+	T items[MAX];
+	int top;
+public:
+	Stack();
+	bool isempty();
+	bool isfull();
+	bool push(const T &item);
+	bool pop(T &item);
+	~Stack();
+};
+
+template <class T>
+Stack<T>::Stack()
+{
+	top = 0;
+}
+
+template <class T>
+bool Stack<T>::isempty()
+{
+	return top == 0;
+}
+
+template <class T>
+bool Stack<T>::isfull()
+{
+	return top == MAX;
+}
+
+template <class T>
+bool Stack<T>::push(const T &item)
+{
+	if(top<MAX)
+	{
+		items[top++]=item;
+		return true;
+	}
+	else
+		return false;
+}
+
+template <class T>
+bool Stack<T>::pop(T &item)
+{
+	if(top>0)
+	{
+		item = items[--top];
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+#endif
